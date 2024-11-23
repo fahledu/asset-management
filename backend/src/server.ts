beaf717b4934f421a54a91a8aa3d01bff62e20c8
fahledu <fahledu@gmail.com>
2024-11-23 16:22:45 -0300

    chore: install and configure Express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello word")
+import express, { json, Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json())
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({asset: "tablet"});
+});
+
+app.listen(port, () => {
+    console.log(`Server runnig at port: ${port}`)
+})
