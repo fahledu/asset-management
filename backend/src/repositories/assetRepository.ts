0a6a14b43b748648632c54d3aa65b51f6f11133e
fahledu <fahledu@gmail.com>
2024-11-24 11:56:18 -0300

    feat: add repository layer

---
+++ backend/src/repositories/assetRepository.ts
@@ -0,0 +1,17 @@
+interface AssetModel {
+    id: number;
+    name: string;
+}
+
+const database: AssetModel[] = [
+    { id: 1, name: "tablet" },
+    { id: 2, name: "impressora" }
+];
+
+export const findAllAssets = async (): Promise<AssetModel[]> => {
+    return database;
+};
+
+export const findAssetById = async (id: number): Promise<AssetModel | undefined> => {
+    return database.find(asset => asset.id === id )
+}
