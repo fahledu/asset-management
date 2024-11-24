0c0500a6fc29cf8c7a3d82e172e3e3b192405bc5
fahledu <fahledu@gmail.com>
2024-11-24 06:40:39 -0300

    refactor: separate server logic from app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,9 @@
-import express, { json, Request, Response } from "express";
+import createApp from "./app";
 
-const app = express();
+const app = createApp();
 const port = process.env.PORT;
 
-app.use(json())
-
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({asset: "tablet"});
-});
 
 app.listen(port, () => {
     console.log(`Server runnig at port: ${port}`)
-})
+});
