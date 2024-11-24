48c7b735b83bf54e4b37740040117f8108ac9ed2
fahledu <fahledu@gmail.com>
2024-11-24 07:03:11 -0300

    refactor: update assetController to use services layer

--- backend/src/controllers/assetController.ts
+++ backend/src/controllers/assetController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { getAssetService } from "../services/assetService";
 
-export const getAsset = (req: Request, res: Response) => {
-    res.status(200).json({ asset: "impressora" });
-}
+export const getAsset = async (req: Request, res: Response) => {
+    const data = await getAssetService();
+    res.status(200).json(data);
+} 
