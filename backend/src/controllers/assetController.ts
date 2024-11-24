ac6f80931173b45c5a734bdb59e5b6246199a8d6
fahledu <fahledu@gmail.com>
2024-11-24 07:22:36 -0300

    refactor: update assetControler to use utils layer

--- backend/src/controllers/assetController.ts
+++ backend/src/controllers/assetController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import { getAssetService } from "../services/assetService";
+import { ok } from "../utils/httpHelper";
 
 export const getAsset = async (req: Request, res: Response) => {
     const data = await getAssetService();
-    res.status(200).json(data);
+    const response = await ok(data);
+
+    res.status(response.statusCode).json(response.body);
 } 
