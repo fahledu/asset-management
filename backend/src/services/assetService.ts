e56cd488a4900ebcd48f085611a3cf2457d20981
fahledu <fahledu@gmail.com>
2024-11-24 11:57:46 -0300

    refactor: update assetService to use repository layer

--- backend/src/services/assetService.ts
+++ backend/src/services/assetService.ts
@@ -1,9 +1,10 @@
+import * as AssetRepository from "../repositories/assetRepository";
 import { noContent, ok } from "../utils/httpHelper";
 
 export const getAssetService = async () => {
     let response = null;
 
-    const data = { asset: "impressora" };
+    const data = await AssetRepository.findAllAssets();
     
     if (data) {
         response = await ok(data);
